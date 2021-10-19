c73cae48d0b48d62115ccee4fc6bffecb2a11777
Edgar Andrés Margffoy Tuay <andfoy@gmail.com>
2021-10-19 14:29:53 -0500

    Disable SSE on aarch64

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-./configure --prefix=${PREFIX} --enable-sse
+EXTRA_FLAGS="--enable-sse"
+if [[ ${target_platform} == "linux-aarch64" ]]; then
+  EXTRA_FLAGS=""
+fi
+
+./configure --prefix=${PREFIX} $EXTRA_FLAGS
 make
 make check
 make install
